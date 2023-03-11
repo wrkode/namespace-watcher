c4494aacb188d9c258ef435ae1cdccd33dca3dee
wrkode <william.rizzo@suse.com>
2023-03-11 08:55:33 +0100

    removed indexer watch and switched to event stream

--- app/namespace-watcher.go
+++ app/namespace-watcher.go
@@ -4,95 +4,106 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
+	"strings"
 
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 )
 
-//var cpuLimit string
-//var memLimit string
-//var ephemeralStorageLimit string
+func createLimitRange(clientset *kubernetes.Clientset, namespaceName string) error {
 
-func main() {
+	cpuLimitMax := os.Getenv("CPU_LIMIT_MAX")
+	memoryLimitMax := os.Getenv("MEM_LIMIT_MAX")
+	ephemeralStorageLimitMax := os.Getenv("EPHEMERAL_STORAGE_MAX")
 
-	cpuLimit := os.Getenv("CPU_LIMIT")
-	memLimit := os.Getenv("MEM_LIMIT")
-	ephemeralStorageLimit := os.Getenv("EPHEMERAL_STORAGE_LIMIT")
+	cpuLimitMin := os.Getenv("CPU_LIMIT_MIN")
+	memoryLimitMin := os.Getenv("MEM_LIMIT_MAX")
+	ephemeralStorageLimitMin := os.Getenv("EPHEMERAL_STORAGE_MAX")
 
-	fmt.Printf("CPU Limit: %v\n", cpuLimit)
-	fmt.Printf("MEM Limit: %v\n", memLimit)
-	fmt.Printf("EPHEMERAL STORAGE Limit: %v\n", ephemeralStorageLimit)
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	namespaceInformer := cache.NewSharedIndexInformer(
-		cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "namespaces", metav1.NamespaceAll, nil),
-		&v1.Namespace{},
-		0,
-		cache.Indexers{},
-	)
-
-	namespaceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			namespace := obj.(*v1.Namespace)
-
-			fmt.Printf("Namespace added: %s\n", namespace.Name)
-
-			limitRange := &v1.LimitRange{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "default-limits",
-					Namespace: namespace.Name,
-				},
-				Spec: v1.LimitRangeSpec{
-					Limits: []v1.LimitRangeItem{
-						{
-							Type: v1.LimitTypeContainer,
-							Default: v1.ResourceList{
-								v1.ResourceMemory: resource.MustParse(memLimit),
-								v1.ResourceCPU:    resource.MustParse(cpuLimit),
-							},
-							Max: v1.ResourceList{
-								v1.ResourceMemory: resource.MustParse(ephemeralStorageLimit),
-							},
-						},
+	limitRange := &corev1.LimitRange{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "default-limits",
+		},
+		Spec: corev1.LimitRangeSpec{
+			Limits: []corev1.LimitRangeItem{
+				{
+					Type: corev1.LimitTypePod,
+					Max: corev1.ResourceList{
+						corev1.ResourceCPU:              resource.MustParse(cpuLimitMax),
+						corev1.ResourceMemory:           resource.MustParse(memoryLimitMax),
+						corev1.ResourceEphemeralStorage: resource.MustParse(ephemeralStorageLimitMax),
+					},
+					Min: corev1.ResourceList{
+						corev1.ResourceCPU:              resource.MustParse(cpuLimitMin),
+						corev1.ResourceMemory:           resource.MustParse(memoryLimitMin),
+						corev1.ResourceEphemeralStorage: resource.MustParse(ephemeralStorageLimitMin),
 					},
 				},
-			}
-
-			fmt.Printf("Creating LimitRange for Namespace %s: %+v\n", namespace.Name, limitRange)
-
-			_, err := clientset.CoreV1().LimitRanges(namespace.Name).Create(context.Background(), limitRange, metav1.CreateOptions{})
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating LimitRange for Namespace %s: %v", namespace.Name, err)
-			} else {
-				fmt.Printf("LimitRange created for Namespace %s\n", namespace.Name)
-			}
+			},
 		},
-	})
-
-	stopCh := make(chan struct{})
-	defer close(stopCh)
-
-	go namespaceInformer.Run(stopCh)
-	fmt.Printf("Namespace Informer: %+v\n", namespaceInformer)
-
-	if !cache.WaitForCacheSync(stopCh, namespaceInformer.HasSynced) {
-		panic("Timed out waiting for caches to sync")
 	}
 
-	wait.Forever(func() { time.Sleep(time.Minute) }, time.Second)
+	_, err := clientset.CoreV1().LimitRanges(namespaceName).Create(context.Background(), limitRange, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Created LimitRange for namespace %s\n", namespaceName)
+	return nil
+}
+
+func main() {
+	// create Kubernetes API client
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		fmt.Println("Failed to get Kubernetes config:", err)
+		os.Exit(1)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Println("Failed to create Kubernetes client:", err)
+		os.Exit(1)
+	}
+
+	// watch for namespace creation events
+	watcher, err := clientset.CoreV1().Namespaces().Watch(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Println("Failed to watch namespaces:", err)
+		os.Exit(1)
+	}
+
+	// process events
+	for event := range watcher.ResultChan() {
+		// check if event is a namespace creation event
+		if event.Type == watch.Added {
+			// print namespace name to STDOUT
+			namespaceName := event.Object.(*corev1.Namespace).ObjectMeta.Name
+
+			//exclude cattle and system namespaces
+			if strings.Contains(namespaceName, "default") {
+				fmt.Printf("Skipping namespace %s\n", namespaceName)
+				continue
+			}
+			if strings.Contains(namespaceName, "cattle") || strings.Contains(namespaceName, "kube-system") || strings.Contains(namespaceName, "kube-public") {
+				fmt.Printf("Skipping namespace %s\n", namespaceName)
+				continue
+			}
+			if strings.Contains(namespaceName, "istio-system") || strings.Contains(namespaceName, "kube-public") || strings.Contains(namespaceName, "kube-local") {
+				fmt.Printf("Skipping namespace %s\n", namespaceName)
+				continue
+			}
+
+			fmt.Printf("New namespace created: %s\n", namespaceName)
+
+			// create a LimitRange for the new namespace
+			err := createLimitRange(clientset, namespaceName)
+			if err != nil {
+				fmt.Printf("Failed to create LimitRange for namespace %s: %v\n", namespaceName, err)
+			}
+		}
+	}
 }
